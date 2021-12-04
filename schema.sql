e80fd040194c3e3481046154475627727bfdb7f9
Rafael Oliveira <rafaeldco@hotmail.com>
2021-12-03 23:39:58 -0300

    Arquivo sql contendo a configuração das tabelas

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE hours_control;
+
+DROP TABLE IF EXISTS squad CASCADE;
+
+CREATE TABLE squad (
+  id serial PRIMARY KEY,
+  name varchar(50) NOT NULL
+ );
+
+DROP TABLE IF EXISTS usuario CASCADE;
+
+CREATE TABLE usuario (
+  id serial PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  user_estimated_hours int DEFAULT 0,
+  squad_id int REFERENCES squad(id) NOT NULL
+ );
+
+DROP TABLE IF EXISTS report CASCADE;
+
+CREATE TABLE report (
+  id serial PRIMARY KEY,
+  description text NOT NULL,
+  user_id int REFERENCES usuario(id) NOT NULL,
+  spent_hours int NOT NULL,
+  created_at date DEFAULT CURRENT_TIMESTAMP
+);
