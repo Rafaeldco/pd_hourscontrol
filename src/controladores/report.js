d58d3d3e815c979e26c3e14cda3af221ad3daeec
Rafael Oliveira <rafaeldco@hotmail.com>
2021-12-03 23:36:53 -0300

    Criação e configuração do controlador de report

---
+++ src/controladores/report.js
@@ -0,0 +1,30 @@
+const knex = require('../conexao');
+const { schemaCadastrarReport } = require('../schema/schemaReport');
+
+const cadastrarReport = async (req, res) => {
+    const { description, user_id, spent_hours } = req.body;
+
+    try {
+        await schemaCadastrarReport.validate(req.body);
+
+        const validarUsuario = await knex('usuario').where('id', user_id).first();
+
+        if (!validarUsuario) {
+            return res.status(404).json({ mensagem: 'Usuário não encontrado.' });
+        }
+
+        const cadastroReport = await knex('report').insert({ description, user_id, spent_hours }).returning('*');
+        const atualizarHoras = await knex('usuario').where('id', user_id).increment('user_estimated_hours', spent_hours);
+
+        if (!cadastroReport) {
+            return res.status(400).json({ mensagem: 'Não foi possível inserir o report.' });
+        }
+
+        res.status(201).json({ mensagem: 'Report feito com sucesso!' });
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = { cadastrarReport }
